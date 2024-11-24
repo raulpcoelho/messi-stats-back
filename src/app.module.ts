a15fa34269793fd2505c8db391d2340773d48639
Raul Coelho <rpc3@cin.ufpe.br>
2024-11-24 03:19:55 -0300

    fix: deleting unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { MatchesModule } from './matches/matches.module';
 import { SeasonsModule } from './seasons/seasons.module';
@@ -9,7 +7,7 @@ import { TeamsModule } from './teams/teams.module';
 
 @Module({
   imports: [DatabaseModule, MatchesModule, SeasonsModule, CompetitionsModule, TeamsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
