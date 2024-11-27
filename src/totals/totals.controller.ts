a26c1b4b9a920aa14d3d4a0438143ec7b00512f5
Raul Coelho <rpc3@cin.ufpe.br>
2024-11-27 00:24:24 -0300

    test: totals controller tests implemented

--- src/totals/totals.controller.ts
+++ src/totals/totals.controller.ts
@@ -1,17 +1,21 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Query, Res } from '@nestjs/common';
 import { TotalsService } from './totals.service';
-import { FindMatchDto } from 'src/matches/dto/find-match.dto';
+import { FindMatchDto } from '../matches/dto/find-match.dto';
 import { TotalsDto } from './dtos/totals.dto';
+import { Response } from 'express';
 
 @Controller('totals')
 export class TotalsController {
   constructor(private readonly totalsService: TotalsService) {}
   @Get()
-  async findAllFiltered(@Query() findMatchDto: FindMatchDto): Promise<TotalsDto | { error: string }> {
+  async findAllFiltered(@Res() res: Response, @Query() findMatchDto: FindMatchDto): Promise<Response<TotalsDto>> {
     try {
-      return await this.totalsService.findAllFiltered(findMatchDto);
+      const totals: TotalsDto = await this.totalsService.findAllFiltered(findMatchDto);
+      return res.status(HttpStatus.OK).json(totals);
     } catch (error) {
-      return { error: 'An error occurred while trying to fetch totals' };
+      return res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json({ error: 'An error occurred while trying to fetch totals' });
     }
   }
 }
