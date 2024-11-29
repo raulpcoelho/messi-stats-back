f8098eb5d6e6107f43de36eddaa2a4138682b83f
Raul Coelho <rpc3@cin.ufpe.br>
2024-11-29 03:00:20 -0300

    test: adding more e2e tests for matches and totals

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -20,7 +20,7 @@ describe('AppController (e2e)', () => {
     await app.close();
   });
 
-  it('/matches (GET)', async () => {
+  it('/matches (GET): World Cup 2022 filter', async () => {
     const expected = await fs.promises.readFile('test/data/expected/allMatches.json', 'utf-8');
     const expectedJson = JSON.parse(expected);
     await request(app.getHttpServer())
@@ -29,11 +29,33 @@ describe('AppController (e2e)', () => {
       .expect(expectedJson);
   });
 
-  it('/totals (GET)', async () => {
+  it('/matches (GET) - no matches', async () => {
+    await request(app.getHttpServer()).get('/matches?goals=6').expect(200).expect([]);
+  });
+
+  it('/matches (GET): World Cup 2022, 0 goal filter', async () => {
+    const expected = await fs.promises.readFile('test/data/expected/filteredMatches2.json', 'utf-8');
+    const expectedJson = JSON.parse(expected);
+    await request(app.getHttpServer())
+      .get('/matches?year=2022&competition=World%20Cup&goals=0')
+      .expect(200)
+      .expect(expectedJson);
+  });
+
+  it('/totals (GET): World Cup 2022 filter', async () => {
     const expected = await fs.promises.readFile('test/data/expected/totals.json', 'utf-8');
     const expectedJson = JSON.parse(expected);
     await request(app.getHttpServer())
       .get('/totals?year=2022&competition=World%20Cup')
+      .expect(200)
+      .expect(expectedJson);
+  });
+
+  it('/totals (GET): World Cup 2022, 0 goal filter', async () => {
+    const expected = await fs.promises.readFile('test/data/expected/filteredTotals2.json', 'utf-8');
+    const expectedJson = JSON.parse(expected);
+    await request(app.getHttpServer())
+      .get('/totals?year=2022&competition=World%20Cup&goals=0')
       .expect(200)
       .expect(expectedJson);
   });
