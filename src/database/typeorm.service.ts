2bab64fb323d85eb5d73582958b47a259850818e
Raul Coelho <rpc3@cin.ufpe.br>
2024-12-04 03:26:40 -0300

    fix: migrations and seeds env var

--- src/database/typeorm.service.ts
+++ src/database/typeorm.service.ts
@@ -1,15 +1,6 @@
 import { OnModuleInit } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { config } from 'dotenv';
-import { Match } from '../matches/entities/match.entity';
-import { Competition } from '../competitions/entities/competition.entity';
-import { Season } from '../seasons/entities/season.entity';
-import { Team } from '../teams/entities/team.entity';
-import { CreateTables1732337440742 } from './migrations/1732337440742-CreateTables';
-import { Competitions1732338967859 } from './seeds/1732338967859-Competitions';
-import { Seasons1732339620799 } from './seeds/1732339620799-Seasons';
-import { Teams1732339964861 } from './seeds/1732339964861-Teams';
-import { Matches1732340454118 } from './seeds/1732340454118-Matches';
 
 config(); // use dotenv to make sure the environment variables are loaded before using it.
 
@@ -30,14 +21,8 @@ export class TypeOrmService extends DataSource implements OnModuleInit {
           },
         },
       }),
-      entities: [Competition, Match, Season, Team],
-      migrations: [
-        CreateTables1732337440742,
-        Competitions1732338967859,
-        Seasons1732339620799,
-        Teams1732339964861,
-        Matches1732340454118,
-      ],
+      entities: [process.env.TYPEORM_ENTITIES],
+      migrations: [process.env.TYPEORM_MIGRATIONS, process.env.TYPEORM_SEEDS],
       synchronize: false,
       logging: false,
     });
