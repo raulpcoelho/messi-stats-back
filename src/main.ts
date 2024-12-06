a15f529adf1d620e88d7457e0c73d776435d8d9b
Raul Coelho <rpc3@cin.ufpe.br>
2024-12-05 23:29:56 -0300

    fix: api description, license and contact

--- src/main.ts
+++ src/main.ts
@@ -15,11 +15,15 @@ async function bootstrap() {
     }),
   );
 
+  const description =
+    'This is an API that provides detailed statistics for all matches played by Lionel Messi. You can filter the data by year, opponents, season, competition, and others to get specific insights. The data is updated regularly as Messi plays new matches.';
+
   const config = new DocumentBuilder()
     .setTitle('Messi Stats API')
-    .setDescription('Stats for all Messi games')
+    .setDescription(description)
     .setVersion('1.0.0')
-    .setContact('Raul Coelho', null, 'rpc3@cin.ufpe.br')
+    .setContact('Raul Coelho', 'https://www.github.com/raulpcoelho', 'rpc3@cin.ufpe.br')
+    .setLicense('Apache 2.0', 'https://www.apache.org/licenses/LICENSE-2.0')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
